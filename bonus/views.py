7afa9cdb26c84659d7d9f51679aae347fbdd528c
shuhuangso <shuhuang.so@qq.com>
2022-07-04 18:31:24 +0800

    excel coupang finished

--- bonus/views.py
+++ bonus/views.py
@@ -5,6 +5,7 @@ from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from xToolkit import xstring
 
 from bonus.models import AccountSales, AccountBonus, Accounts, MonthList, ExchangeRate, BasicInfo, Manager
 from bonus.serializers import AccountSalesSerializer, AccountBonusSerializer, AccountsSerializer, MonthListSerializer, \
@@ -139,15 +140,22 @@ class AccountBonusViewSet(mixins.ListModelMixin,
     @action(methods=['get'], detail=False, url_path='export_bonus')
     def export_bonus(self, request):
         import openpyxl
-        from openpyxl.styles import Alignment, Font, Color, PatternFill
+        from openpyxl.styles import Alignment, Font, Border, Side, PatternFill
         alignment = Alignment(horizontal='center', vertical='center')
         title_style = Font(name='微软雅黑', sz=28, b=True)
         title_style2 = Font(name='微软雅黑', sz=8, color='ffffff')
+        title_style3 = Font(name='微软雅黑', sz=8)
+        border = Border(
+            left=Side(border_style='thin', color='e7e6e6'),
+            right=Side(border_style='thin', color='e7e6e6'),
+            top=Side(style='thin', color='e7e6e6'),
+            bottom=Side(style='thin', color='e7e6e6'))
 
-        months = ['202206', '202301']
+        months = ['202212', '202301']
         platforms = ['eBay', 'Coupang']
-        manager = '吴梓涵'
+        manager = '韦泳冰'
         area = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+        area2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
         wb = openpyxl.Workbook()
         for m in months:
             print(m)
@@ -155,25 +163,28 @@ class AccountBonusViewSet(mixins.ListModelMixin,
                 if p == 'eBay':
                     # 获取当月ebay平台指定负责人的提成数据
                     account_bonus = AccountBonus.objects.filter(month=m, platform=p, manager__name=manager)
-                    print(account_bonus)
                     if account_bonus:
-                        f_sheet = wb.create_sheet(m + 'eBay')
+                        f_sheet = wb.create_sheet(m[0:4] + '年' + m[4:6] + '月' + 'eBay')
                         num = 0
                         for ab in account_bonus:
                             # 第一次循环
                             if num == 0:
                                 f_sheet.merge_cells('A1:P1')
-                                f_sheet['A1'] = m + 'eBay'
+                                f_sheet['A1'] = m[0:4] + '年' + m[4:6] + '月' + 'eBay销售报表-' + ab.manager.name
+                                # 第1行样式
                                 f_sheet.row_dimensions[1].height = 50
                                 f_sheet.row_dimensions[2].height = 30
                                 f_sheet['A1'].alignment = alignment
                                 f_sheet['A1'].font = title_style
                                 f_sheet['A1'].fill = PatternFill(patternType='solid', fgColor='65d6b4')
+                                for i in area:
+                                    f_sheet[i + '1'].border = border
                                 # 第二行样式
                                 for i in area:
                                     f_sheet[i+'2'].alignment = alignment
                                     f_sheet[i+'2'].font = title_style2
                                     f_sheet[i+'2'].fill = PatternFill(patternType='solid', fgColor='df8244')
+                                    f_sheet[i+'2'].border = border
                                 f_sheet['A2'] = '账号'
                                 f_sheet['B2'] = 'eBay站点'
                                 f_sheet['C2'] = '销售收入'
@@ -191,40 +202,123 @@ class AccountBonusViewSet(mixins.ListModelMixin,
                                 f_sheet['O2'] = '提成(' + str(ab.bonus_rate*100) + '%)'
                                 f_sheet['P2'] = '是否记入提成'
 
+                                # 第3行样式
+                                for i in area:
+                                    f_sheet[i + '3'].alignment = alignment
+                                    f_sheet[i + '3'].font = title_style3
+                                    f_sheet[i + '3'].border = border
                                 f_sheet['A3'] = ab.account_name
                                 f_sheet['B3'] = ab.platform_base
-                                f_sheet['C3'] = ab.sale_amount
-                                f_sheet['D3'] = ab.receipts
-                                f_sheet['E3'] = ab.refund
-                                f_sheet['F3'] = ab.FES
-                                f_sheet['G3'] = ab.platform_fees
-                                f_sheet['H3'] = ab.product_cost
-                                f_sheet['I3'] = ab.shipping_cost
-                                f_sheet['J3'] = ab.profit
+                                f_sheet['C3'] = ab.ori_currency + ' ' + xstring.dispose(ab.sale_amount).humanized_amount(compel=True)
+                                f_sheet['D3'] = ab.ori_currency + ' ' + xstring.dispose(ab.receipts).humanized_amount(compel=True)
+                                f_sheet['E3'] = ab.ori_currency + ' ' + xstring.dispose(ab.refund).humanized_amount(compel=True)
+                                f_sheet['F3'] = '￥' + xstring.dispose(ab.FES).humanized_amount(compel=True)
+                                f_sheet['G3'] = ab.ori_currency + ' ' + xstring.dispose(ab.platform_fees).humanized_amount(compel=True)
+                                f_sheet['H3'] = '￥' + xstring.dispose(ab.product_cost).humanized_amount(compel=True)
+                                f_sheet['I3'] = '￥' + xstring.dispose(ab.shipping_cost).humanized_amount(compel=True)
+                                f_sheet['J3'] = '￥' + xstring.dispose(ab.profit).humanized_amount(compel=True)
                                 f_sheet['K3'] = ab.orders
-                                f_sheet['L3'] = ab.CUP
-                                f_sheet['M3'] = ab.ad_fees
+                                f_sheet['L3'] = ab.ori_currency + ' ' + xstring.dispose(ab.CUP).humanized_amount(compel=True)
+                                f_sheet['M3'] = ab.ori_currency + ' ' + xstring.dispose(ab.ad_fees).humanized_amount(compel=True)
                                 f_sheet['N3'] = ab.ad_percent
-                                f_sheet['O3'] = ab.bonus
+                                f_sheet['O3'] = '￥' + xstring.dispose(ab.bonus).humanized_amount(compel=True)
                                 f_sheet['P3'] = '是' if ab.bonus >= 500 else '否'
                             elif num > 0:
+                                # >=4行样式
+                                for i in area:
+                                    f_sheet[i + str(num+3)].alignment = alignment
+                                    f_sheet[i + str(num+3)].font = title_style3
+                                    f_sheet[i + str(num+3)].border = border
                                 f_sheet['A'+str(num+3)] = ab.account_name
                                 f_sheet['B'+str(num+3)] = ab.platform_base
-                                f_sheet['C'+str(num+3)] = ab.sale_amount
-                                f_sheet['D'+str(num+3)] = ab.receipts
-                                f_sheet['E'+str(num+3)] = ab.refund
-                                f_sheet['F'+str(num+3)] = ab.FES
-                                f_sheet['G'+str(num+3)] = ab.platform_fees
-                                f_sheet['H'+str(num+3)] = ab.product_cost
-                                f_sheet['I'+str(num+3)] = ab.shipping_cost
-                                f_sheet['J'+str(num+3)] = ab.profit
+                                f_sheet['C'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.sale_amount).humanized_amount(compel=True)
+                                f_sheet['D'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.receipts).humanized_amount(compel=True)
+                                f_sheet['E'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.refund).humanized_amount(compel=True)
+                                f_sheet['F'+str(num+3)] = '￥' + xstring.dispose(ab.FES).humanized_amount(compel=True)
+                                f_sheet['G'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.platform_fees).humanized_amount(compel=True)
+                                f_sheet['H'+str(num+3)] = '￥' + xstring.dispose(ab.product_cost).humanized_amount(compel=True)
+                                f_sheet['I'+str(num+3)] = '￥' + xstring.dispose(ab.shipping_cost).humanized_amount(compel=True)
+                                f_sheet['J'+str(num+3)] = '￥' + xstring.dispose(ab.profit).humanized_amount(compel=True)
                                 f_sheet['K'+str(num+3)] = ab.orders
-                                f_sheet['L'+str(num+3)] = ab.CUP
-                                f_sheet['M'+str(num+3)] = ab.ad_fees
+                                f_sheet['L'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.CUP).humanized_amount(compel=True)
+                                f_sheet['M'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.ad_fees).humanized_amount(compel=True)
                                 f_sheet['N'+str(num+3)] = ab.ad_percent
-                                f_sheet['O'+str(num+3)] = ab.bonus
+                                f_sheet['O'+str(num+3)] = '￥' + xstring.dispose(ab.bonus).humanized_amount(compel=True)
                                 f_sheet['P'+str(num+3)] = '是' if ab.bonus >= 500 else '否'
+                            num += 1
+                if p == 'Coupang':
+                    # 获取当月Coupang平台指定负责人的提成数据
+                    account_bonus = AccountBonus.objects.filter(month=m, platform=p, manager__name=manager)
+                    if account_bonus:
+                        f_sheet = wb.create_sheet(m[0:4] + '年' + m[4:6] + '月' + 'Coupang')
+                        num = 0
+                        for ab in account_bonus:
+                            # 第一次循环
+                            if num == 0:
+                                f_sheet.merge_cells('A1:L1')
+                                f_sheet['A1'] = m[0:4] + '年' + m[4:6] + '月' + 'Coupang销售报表-' + ab.manager.name
+                                # 第1行样式
+                                f_sheet.row_dimensions[1].height = 50
+                                f_sheet.row_dimensions[2].height = 30
+                                f_sheet['A1'].alignment = alignment
+                                f_sheet['A1'].font = title_style
+                                f_sheet['A1'].fill = PatternFill(patternType='solid', fgColor='65d6b4')
+                                for i in area2:
+                                    f_sheet[i + '1'].border = border
+                                # 第二行样式
+                                for i in area2:
+                                    f_sheet[i+'2'].alignment = alignment
+                                    f_sheet[i+'2'].font = title_style2
+                                    f_sheet[i+'2'].fill = PatternFill(patternType='solid', fgColor='df8244')
+                                    f_sheet[i+'2'].border = border
+                                f_sheet['A2'] = '账号'
+                                f_sheet['B2'] = '站点'
+                                f_sheet['C2'] = '销售收入'
+                                f_sheet['D2'] = '销售手续费'
+                                f_sheet['E2'] = 'CGF/CGF LITE Fees'
+                                f_sheet['F2'] = '最终支付金额'
+                                f_sheet['G2'] = '结汇收入'
+                                f_sheet['H2'] = '产品成本'
+                                f_sheet['I2'] = 'CGF发仓运费'
+                                f_sheet['J2'] = '毛利润'
+                                f_sheet['K2'] = '提成(' + str(ab.bonus_rate*100) + '%)'
+                                f_sheet['L2'] = '是否记入提成'
 
+                                # 第3行样式
+                                for i in area2:
+                                    f_sheet[i + '3'].alignment = alignment
+                                    f_sheet[i + '3'].font = title_style3
+                                    f_sheet[i + '3'].border = border
+                                f_sheet['A3'] = ab.account_name
+                                f_sheet['B3'] = ab.platform_base
+                                f_sheet['C3'] = ab.ori_currency + ' ' + xstring.dispose(ab.sale_amount).humanized_amount(compel=True)
+                                f_sheet['D3'] = ab.ori_currency + ' ' + xstring.dispose(ab.platform_fees).humanized_amount(compel=True)
+                                f_sheet['E3'] = ab.ori_currency + ' ' + xstring.dispose(ab.cp_cgf_fees).humanized_amount(compel=True)
+                                f_sheet['F3'] = ab.ori_currency + ' ' + xstring.dispose(ab.receipts).humanized_amount(compel=True)
+                                f_sheet['G3'] = '￥' + xstring.dispose(ab.FES).humanized_amount(compel=True)
+                                f_sheet['H3'] = '￥' + xstring.dispose(ab.product_cost).humanized_amount(compel=True)
+                                f_sheet['I3'] = '￥' + xstring.dispose(ab.cp_first_ship).humanized_amount(compel=True)
+                                f_sheet['J3'] = '￥' + xstring.dispose(ab.profit).humanized_amount(compel=True)
+                                f_sheet['K3'] = '￥' + xstring.dispose(ab.bonus).humanized_amount(compel=True)
+                                f_sheet['L3'] = '是' if ab.bonus >= 500 else '否'
+                            elif num > 0:
+                                # >=4行样式
+                                for i in area2:
+                                    f_sheet[i + str(num+3)].alignment = alignment
+                                    f_sheet[i + str(num+3)].font = title_style3
+                                    f_sheet[i + str(num+3)].border = border
+                                f_sheet['A'+str(num+3)] = ab.account_name
+                                f_sheet['B'+str(num+3)] = ab.platform_base
+                                f_sheet['C'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.sale_amount).humanized_amount(compel=True)
+                                f_sheet['D'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.platform_fees).humanized_amount(compel=True)
+                                f_sheet['E'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.cp_cgf_fees).humanized_amount(compel=True)
+                                f_sheet['F'+str(num+3)] = ab.ori_currency + ' ' + xstring.dispose(ab.receipts).humanized_amount(compel=True)
+                                f_sheet['G'+str(num+3)] = '￥' + xstring.dispose(ab.FES).humanized_amount(compel=True)
+                                f_sheet['H'+str(num+3)] = '￥' + xstring.dispose(ab.product_cost).humanized_amount(compel=True)
+                                f_sheet['I'+str(num+3)] = '￥' + xstring.dispose(ab.cp_first_ship).humanized_amount(compel=True)
+                                f_sheet['J'+str(num+3)] = '￥' + xstring.dispose(ab.profit).humanized_amount(compel=True)
+                                f_sheet['K'+str(num+3)] = '￥' + xstring.dispose(ab.bonus).humanized_amount(compel=True)
+                                f_sheet['L'+str(num+3)] = '是' if ab.bonus >= 500 else '否'
                             num += 1
 
         # first_sheet = wb.active

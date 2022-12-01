7f7aef3b01c36df55912c016c127f4694715e529
shuhuangso <shuhuang.so@qq.com>
2022-12-01 23:28:09 +0800

    add readme

--- casedance/celery.py
+++ casedance/celery.py
@@ -50,10 +50,6 @@ app.conf.beat_schedule = {
         'task': 'bonus.tasks.create_month',  # 生成当月月份
         'schedule': crontab(minute=0, hour=1, day_of_month=1),  # 每月1号执行
     },
-    'delete_logs': {
-        'task': 'product.tasks.delete_logs',  # 删除1个月前任务执行日志
-        'schedule': crontab(minute=0, hour=2, day_of_month=1),  # 每月1号执行
-    },
     'track_listing': {
         'task': 'mercado.tasks.track_listing',  # 每天更新链接信息并进行销量计算
         'schedule': crontab(hour='0', minute='3'),  # 每天0点1分开始计算
@@ -61,6 +57,14 @@ app.conf.beat_schedule = {
     'track_seller': {
         'task': 'mercado.tasks.track_listing',  # 每天更新卖家信息并进行销量计算
         'schedule': crontab(hour='0', minute='30'),  # 每天0点30分开始计算
+    },
+    'delete_logs': {
+        'task': 'product.tasks.delete_logs',  # 删除1个月前任务执行日志
+        'schedule': crontab(minute=0, hour=2, day_of_month=1),  # 每月1号执行
+    },
+    'fbm_product_sales': {
+        'task': 'mercado.tasks.calc_product_sales',  # 每天更新fbm产品销量信息计算
+        'schedule': crontab(hour='0', minute='40'),  # 每天0点40分开始计算
     },
 }
 # 自动从所有已注册的django app中加载任务
